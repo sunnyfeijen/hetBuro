c0552f83d4206b45391776b4af8e2e28d1cc1a58
BartvanVegchel <bart.vanvegchel@hotmail.com>
2015-05-25 16:21:45 +0200

    event tekst toegevoegd

--- src/event.php
+++ src/event.php
@@ -7,14 +7,18 @@
     <body>
     	<section class='about_description'>
     		<h2>Over het Event</h2>
-    		<h3></h3>
+    		<h3>Tijdens Het eindevent van semester 4 presenteren alle groepen hun werk.</h3>
     		<p>
-   			
+                Als afsluiting van semester 4 "het Buro" word een eind event georganiseerd. 
+                Tijdens dit event presenteren alle proftaakgroepen het resultaat van het project waar ze dit semester aan hebben gewerkt.
+                <br><br>15 groepen uit zowel Eindhoven als Tilburg zullen in het Temporary Art Centrum (TAC) hun presentatie geven.
+                Dit doen ze d.m.v. een A2 poster en een computerscherm. 
    			</p>
 
    			<p>
-
+                Het event zal plaats vinden op 22 juni 2015 van 13:00. De locatie is de eno zaal in het TAC, te Eindhoven.
    			</p>
+
     	</section>
     </body>
 </html>
