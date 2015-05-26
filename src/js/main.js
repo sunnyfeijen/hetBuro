0a4fc25f816680fb852405c4f81135224a6a4a2b
BartvanVegchel <bart.vanvegchel@hotmail.com>
2015-05-26 15:24:11 +0200

    begin hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     
     $( ".triangle" ).click(function() {
@@ -6,4 +7,23 @@ $(document).ready(function(){
       });
     });
     
+});
+
+$( document ).ready(function() {
+    
+    $( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+		});
+	});
+
+	$( ".cross" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+		$( ".cross" ).hide();
+		$( ".hamburger" ).show();
+		});
+	});
 });
