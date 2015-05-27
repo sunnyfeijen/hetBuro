cc5a1f9a0f6d6d90388c1469b649d560e15d5ab5
aline <aline.swinkels@hotmail.com>
2015-05-27 13:43:35 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 93ede044e56962f226ec8a75b618bd9fce7554cf.

---
+++ src/js/main.js
@@ -0,0 +1,75 @@
+<<<<<<< HEAD
+=======
+$(document).ready(function(){
+    
+    
+//-----Shephard-----------------------------------
+
+
+    //  if first form is shown -->
+    var tour;
+
+    tour = new Shepherd.Tour({
+        defaults: {
+            classes: 'shepherd-theme-arrows'
+        }
+    });
+    
+
+    tour.addStep('any-name', { 
+        text: 'upload your screenshot here.',
+        attachTo: '#firstform #bestand right',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next
+            }
+        ]
+    });
+
+    tour.addStep('b-step', {
+        text: 'Select you class over here',
+        attachTo: '#firstform .klas right',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next
+            }
+        ]
+    });
+    
+    tour.addStep('b-step', {
+        text: 'Type your portfolios url',
+        attachTo: '#firstform .url right',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next
+            }
+        ]
+    });
+    
+    tour.addStep('b-step', {
+        text: 'Press upload and youre done!',
+        attachTo: '#firstform .upload right',
+        buttons: [
+            {
+                text: 'Got it!',
+                action: tour.next
+            }
+        ]
+    });
+
+    tour.start();
+    
+//-----Shephard-END-------------------------------
+    
+    $( ".triangle" ).click(function() {
+        // quit Shepherd
+        tour.complete();
+      $( ".memberarea" ).slideUp( "slow", function() {
+      });
+    });
+    
+});
+>>>>>>> origin/master
