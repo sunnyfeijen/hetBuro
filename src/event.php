65ce5083f85ad655ef9640ad88364b169d153c41
monicavogels <monicavogels@live.nl>
2015-05-27 15:31:21 +0200

    Event en Over het buro responsive

--- src/event.php
+++ src/event.php
@@ -9,17 +9,18 @@
             <div id='kop_blok'></div>
     		<h2>Over het Event</h2>
     		<h3>Tijdens Het eindevent van semester 4 presenteren alle groepen hun werk.</h3>
-    		<p>
-                Als afsluiting van semester 4 "het Buro" word een eind event georganiseerd. 
-                Tijdens dit event presenteren alle proftaakgroepen het resultaat van het project waar ze dit semester aan hebben gewerkt.
-                <br><br>15 groepen uit zowel Eindhoven als Tilburg zullen in het Temporary Art Centrum (TAC) hun presentatie geven.
-                Dit doen ze d.m.v. een A2 poster en een computerscherm. 
-   			</p>
+    		<div id='tekst'>
+                <p>
+                    Als afsluiting van semester 4 "het Buro" word een eind event georganiseerd. 
+                    Tijdens dit event presenteren alle proftaakgroepen het resultaat van het project waar ze dit semester aan hebben gewerkt.
+                    <br><br>15 groepen uit zowel Eindhoven als Tilburg zullen in het Temporary Art Centrum (TAC) hun presentatie geven.
+                    Dit doen ze d.m.v. een A2 poster en een computerscherm. 
+       			</p>
 
-   			<p>
-                Het event zal plaats vinden op 22 juni 2015 van 13:00. De locatie is de eno zaal in het TAC, te Eindhoven.
-   			</p>
-
+       			<p>
+                    Het event zal plaats vinden op 22 juni 2015 van 13:00. De locatie is de eno zaal in het TAC, te Eindhoven.
+       			</p>
+            <div id='tekst'>
     	</section>
     </body>
 </html>
--- src/over.php
+++ src/over.php
@@ -9,22 +9,22 @@
     		<div id='kop_blok'></div><h2>Over het Buro</h2>
     		<h3>Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ aan de slag voor een echte opdrachtgever.</h3>
     		<div id='tekst'>
-            <p>
-    			Semester 4 aan de opleiding ICT en Media Design aan de Fontys heet het Buro. 
-    			Het Buro is opgebouwd uit één proftaak en flankerend onderwijs in de vorm van workshops. 
-    			Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ 
-    			aan de slag voor een echte opdrachtgever. 
-				<br/><br/>
-				Elk ‘buro’ gaat een cross-media campagne met online focus ontwerpen en realiseren voor de opdrachtgever. <br/>
-				
-			</p>
-			<p>
-				In dit semester staan <b>scrum</b>, <b>strategie en concept</b>, <b>campaigning</b>, 
-				<b>user centered design</b>, <b>user experience</b>, <b>JavaScript frameworks</b> en <b>SEO</b>.
-			</br></br>
-	            Omdat het Buro semester gewijs onderwijs is, is er meer tijd om de Scrum methodiek onder de knie te krijgen. 
-				Daarnaast zijn alle leerlijnen met elkaar in samenhang ontwikkeld en dragen ze zo allemaal bij aan een eindresultaat om trots op te zijn. 
-			</p>
+                <p>
+        			Semester 4 aan de opleiding ICT en Media Design aan de Fontys heet het Buro. 
+        			Het Buro is opgebouwd uit één proftaak en flankerend onderwijs in de vorm van workshops. 
+        			Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ 
+        			aan de slag voor een echte opdrachtgever. 
+    				<br/><br/>
+    				Elk ‘buro’ gaat een cross-media campagne met online focus ontwerpen en realiseren voor de opdrachtgever. <br/>
+    				
+    			</p>
+    			<p>
+    				In dit semester staan <b>scrum</b>, <b>strategie en concept</b>, <b>campaigning</b>, 
+    				<b>user centered design</b>, <b>user experience</b>, <b>JavaScript frameworks</b> en <b>SEO</b>.
+    			</br></br>
+    	            Omdat het Buro semester gewijs onderwijs is, is er meer tijd om de Scrum methodiek onder de knie te krijgen. 
+    				Daarnaast zijn alle leerlijnen met elkaar in samenhang ontwikkeld en dragen ze zo allemaal bij aan een eindresultaat om trots op te zijn. 
+    			</p>
             </div>
             <div id='logo_images'>
                 
