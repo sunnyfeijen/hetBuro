c57b58dab5563223274ddd42fc151b7e0488ac66
sunnyfeijen <Sunny@MacBook-Pro-van-Sunny.local>
2015-05-28 14:46:24 +0200

    Small fixes

--- src/over.php
+++ src/over.php
@@ -10,20 +10,15 @@
     		<h3>Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ aan de slag voor een echte opdrachtgever.</h3>
     		<div id='tekst'>
                 <p>
-        			Semester 4 aan de opleiding ICT en Media Design aan de Fontys heet het Buro. 
-        			Het Buro is opgebouwd uit één proftaak en flankerend onderwijs in de vorm van workshops. 
-        			Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ 
-        			aan de slag voor een echte opdrachtgever. 
+        			Semester 4 aan de opleiding ICT en Media Design aan de Fontys heet het Buro. Het Buro is opgebouwd uit één proftaak en flankerend onderwijs in de vorm van workshops. Tijdens dit semester gaan studenten in projectgroepen van 4 tot 6 personen als een ‘buro’ aan de slag voor een echte opdrachtgever. 
     				<br/><br/>
-    				Elk ‘buro’ gaat een cross-media campagne met online focus ontwerpen en realiseren voor de opdrachtgever. <br/>
+    				Elk ‘buro’ gaat een cross-media campagne met online focus ontwerpen en realiseren voor de opdrachtgever.  <br/>
     				
     			</p>
     			<p>
-    				In dit semester staan <b>scrum</b>, <b>strategie en concept</b>, <b>campaigning</b>, 
-    				<b>user centered design</b>, <b>user experience</b>, <b>JavaScript frameworks</b> en <b>SEO</b>.
+    				In dit semester staan scrum, strategie en concept,campaigning, user centered design, user experience,JavaScript frameworks en SEO centraal.
     			</br></br>
-    	            Omdat het Buro semester gewijs onderwijs is, is er meer tijd om de Scrum methodiek onder de knie te krijgen. 
-    				Daarnaast zijn alle leerlijnen met elkaar in samenhang ontwikkeld en dragen ze zo allemaal bij aan een eindresultaat om trots op te zijn. 
+    	            Omdat het Buro semester gewijs onderwijs is, is er meer tijd om de Scrum methodiek onder de knie te krijgen. Daarnaast zijn alle leerlijnen met elkaar in samenhang ontwikkeld en dragen ze zo allemaal bij aan een eindresultaat om trots op te zijn. 
     			</p>
             </div>
             <div id='logo_images'>
