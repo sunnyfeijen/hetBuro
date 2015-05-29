18e0f9fc67dd1287c5b1aa0b476907cfaf7f7f02
BartvanVegchel <bart.vanvegchel@hotmail.com>
2015-05-29 14:50:07 +0200

    google maps toegevoegd aan "het event"

--- src/event.php
+++ src/event.php
@@ -3,11 +3,37 @@
     $thisPage="event";
     include "navigation.php";
 ?>
+    <script>
+
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(51.44112, 5.46966);
+      var mapOptions = {
+        zoom: 12,
+        center: myLatlng
+      }
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'TAC'
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+
 
     <body>
     	<section class='about_description'>
             <img src="img/poster.jpg" id='poster'>
+
+            <section id="map-canvas">
+
+            </section>
     	</section>
+        
     </body>
 </html>
 
